dbde99dbadf811563a5ff99b392a0c1487491d64
Syarif Hidayat <Syarif_H657@mitrais.com>
2019-03-08 10:08:03 +0700

    add how to display data in table format in React

--- src/App.js
+++ src/App.js
@@ -2,17 +2,65 @@ import React, { Component } from 'react';
 import './App.css';
 
 class LeftContent extends Component {
+    constructor(){
+        super();
+        this.state = {
+            data:
+            [
+                {
+                    "id":1,
+                    "name": "John Lennon",
+                    "salary": 55000
+                },
+                {
+                    "id":2,
+                    "name": "Paul McCartney",
+                    "salary": 55000
+                },
+                {
+                    "id":3,
+                    "name": "Goerge Harisson",
+                    "salary": 55000
+                },
+                {
+                    "id":4,
+                    "name": "Ringgo Star",
+                    "salary": 55000
+                }
+
+            ]
+        }
+
+    }
   render() {
     return (
         <div>
             <div className="card">
                 <div className="card-header">Left Side</div>
-                <div className="card-body">Content</div>
+                <div className="card-body">
+                    <table>
+                        <tbody>
+                            {this.state.data.map((person, i) => <TableRow key = {i} data = {person} />)}
+                        </tbody>
+                    </table>
+                </div>
                 <div className="card-footer">Footer</div>
             </div>
         </div>
     );
   }
+}
+
+class TableRow extends React.Component{
+    render() {
+        return (
+          <tr>
+              <td>{this.props.data.id}</td>
+              <td>{this.props.data.name}</td>
+              <td>{this.props.data.salary}</td>
+          </tr>
+        );
+    }
 }
 
 export default LeftContent;
