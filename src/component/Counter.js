69425ff8cff23bd622efbe7eee8fc60db04087dd
Syarif Hidayat <Syarif_H657@mitrais.com>
2019-03-08 15:16:12 +0700

    learning state, event handling and state change

--- src/component/Counter.js
+++ src/component/Counter.js
@@ -2,22 +2,54 @@ import React, { Component } from 'react';
 import '../App.css';
 
 class Counter extends Component {
+
+    constructor(){
+        super();
+        this.handleIncrement = this.handleIncrement.bind(this);
+    }
+
     state = {
-        count: 0
+        count: 0,
+        imageUrl: 'https://picsum.photos/200',
+        tags: ['tag1', 'tag2', 'tag3']
+    };
+
+    styles ={
+      fontSize: 20,
+      fontWeight: "bold"
+    };
+
+    handleIncrement = product =>{
+        console.log(product);
+        this.setState({count: this.state.count + 1})
     }
 
     render() {
+
         return (
            <div>
-               <span>{this.formatCount()}</span>
-               <button>Increment</button>
+               <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
+               <button onClick={() => this.handleIncrement({id: 1})} className="btn btn-secondary btn-sm">Increment</button>
+               { this.renderTags()}
            </div>
         );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
     }
 
     formatCount(){
         const { count } = this.state;
         return count === 0 ? "Zero" : count;
+    }
+
+    renderTags(){
+        if(this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+        return <ul>{this.state.tags.map(tag => <li key={tag}> { tag } </li>)}</ul>;
     }
 }
 export default Counter;
--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import './index.css';
 import Header from './component/Header';
 import LeftContent from './App';
 import RightContent from './component/RightContent';
+import Counter from './component/Counter';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Header />, document.getElementById('header'));
 ReactDOM.render(<LeftContent />, document.getElementById('left-side'));
 ReactDOM.render(<RightContent />, document.getElementById('right-side'));
+ReactDOM.render(<Counter />, document.getElementById('counter'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
