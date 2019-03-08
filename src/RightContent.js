d3d1fb1b7be6884005d9e3549eb5915f7f7c3ee6
Syarif Hidayat <Syarif_H657@mitrais.com>
2019-03-08 09:03:49 +0700

    first commit

---
+++ src/RightContent.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class RightContent extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Right Content</h1>
+                <h2>Content</h2>
+                <p>This is the content</p>
+            </div>
+        );
+    }
+}
+export default RightContent;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import RightContent from './RightContent';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('left-side'));
+ReactDOM.render(<RightContent />, document.getElementById('right-side'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
